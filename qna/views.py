60e3ea4f6aad019aeea03166446e506977422e2c
Mathew Alexander <mathew.alexander@BLR.local>
2020-05-13 20:06:45 +0530

    testing performance

--- qna/views.py
+++ qna/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+from ip2geotools.databases.noncommercial import DbIpCity
+
 
 # Create your views here.
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+
 
 
 def hello_world(request):
-    for i in range(10000000):
-        k=i*i
+    ip=get_client_ip(request)
 
-    return render(request, 'hello_world.html', {"k":k})
+    data = DbIpCity.get(str(ip), api_key='free')
+    address=data.to_json()
+
+
+
+    return render(request, 'hello_world.html', {"address":address})
