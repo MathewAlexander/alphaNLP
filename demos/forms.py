64a5ed70be74096c06dfd9398655eba4834843b6
Mathew Alexander <mathew.alexander@BLR.local>
2020-06-01 23:11:41 +0530

    UI changes

--- demos/forms.py
+++ demos/forms.py
@@ -8,3 +8,9 @@ class QAForm(forms.ModelForm):
     class Meta:
         model = QAData
         fields = ('context', 'question',)
+    def __init__(self, *args, **kwargs):
+            super(QAForm, self).__init__(*args, **kwargs)  # Call to ModelForm constructor
+            self.fields['context'].widget.attrs['cols'] = 100
+            self.fields['context'].widget.attrs['rows'] = 5
+            self.fields['question'].widget.attrs['cols'] = 40
+            self.fields['question'].widget.attrs['rows'] = 1
